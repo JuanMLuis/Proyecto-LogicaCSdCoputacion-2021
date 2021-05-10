dc0aae4651e326ee4e1cde8a4578653d26af4a72
JuanMLuis <juanmaluis98@gmail.com>
2021-05-09 22:15:50 -0300

    Boton
    Cree en boton que cambia el modo de interaccion con la grilla, puede ser # para pintar o X para bloquear (aun no esta hecho lo de no poder pintar en cualquier lado)

--- src/Game.js
+++ src/Game.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PengineClient from './PengineClient';
 import Board from './Board';
 
+
 class Game extends React.Component {
 
   pengine;
@@ -10,11 +11,14 @@ class Game extends React.Component {
     super(props);
     this.state = {
       grid: null,
-      waiting: false
+      waiting: false,
+      mode: '#',
+  
     };
     this.handleClick = this.handleClick.bind(this);
     this.handlePengineCreate = this.handlePengineCreate.bind(this);
     this.pengine = new PengineClient(this.handlePengineCreate);
+    this.cambioDeEstado = this.cambioDeEstado.bind(this);
   }
 
   handlePengineCreate() {
@@ -36,7 +40,7 @@ class Game extends React.Component {
     // Build Prolog query to make the move, which will look as follows:
     // put("#",[0,1],[], [],[["X",_,_,_,_],["X",_,"X",_,_],["X",_,_,_,_],["#","#","#",_,_],[_,_,"#","#","#"]], GrillaRes, FilaSat, ColSat)
     const squaresS = JSON.stringify(this.state.grid).replaceAll('"_"', "_"); // Remove quotes for variables.
-    const queryS = 'put("#", [' + i + ',' + j + ']' 
+    const queryS = 'put(['+this.state.mode+'], [' + i + ',' + j + ']' 
     + ', [], [],' + squaresS + ', GrillaRes, FilaSat, ColSat)';
     this.setState({
       waiting: true
@@ -55,13 +59,29 @@ class Game extends React.Component {
     });
   }
 
+  cambioDeEstado(){
+    if(this.state.mode==='#'){
+      this.setState({mode:'x'})         //investigar poque si la x es mayuscula se cambia por una A solo en la grilla
+    }
+    else this.setState( {mode:'#'})
+  }
+  
+
   render() {
     if (this.state.grid === null) {
       return null;
     }
     const statusText = 'Keep playing!';
     return (
-      <div className="game">
+      <div className="game"> 
+      <div>
+       
+          <button type="submit" class="btn btn-primary btn-sm" onClick={this.cambioDeEstado} >
+        
+            {this.state.mode}  
+            
+         </button> 
+          </div>
         <Board
           grid={this.state.grid}
           onClick={(i, j) => this.handleClick(i,j)}
@@ -69,6 +89,9 @@ class Game extends React.Component {
         <div className="gameInfo">
           {statusText}
         </div>
+        
+        
+        
       </div>
     );
   }
